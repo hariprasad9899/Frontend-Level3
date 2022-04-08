46cb2eb335a3767fb21c924c723a46ad2e7eeac2
hariprasad9899 <hariprasaadsoundararajan@gmail.com>
2022-04-08 22:48:33 +0530

    half done

--- interactive-comments-section-main/app/app.js
+++ interactive-comments-section-main/app/app.js
@@ -23,6 +23,7 @@ for (let i = 0; i < vote.length; i++) {
     }
 }
 
+// adding default comments from json data onload 
 var comment = document.getElementsByClassName('comment');
 window.onload = function() {    
     async function getData() {
@@ -40,11 +41,98 @@ window.onload = function() {
             let user_name = user_img.nextElementSibling;
             let createdAt = user_name.nextElementSibling;
             let user_cmnt = cmnt_text.firstElementChild;
-            user_img.src = x["user"]["image"].png;
+            user_img.src = x["user"]["image"]["png"];
             user_name.innerText = x["user"]["username"];
             createdAt.innerText = x["createdAt"]
             user_cmnt.innerText = x["content"];
+
+
+            if (x.replies.length > 0) {
+                let current_elem = comment[i];
+                for (let i = 0; i < x.replies.length; i++) {
+                    current_elem = current_elem.nextElementSibling;
+                    let user_time = current_elem.getElementsByClassName('user-time')[0];
+                    let cmnt_text = current_elem.getElementsByClassName('cmnt-text')[0];
+                    let p_elem = cmnt_text.getElementsByTagName('p')[0];
+                    let reply_to = p_elem.getElementsByTagName('span')[0];
+                    let user_img = user_time.firstElementChild;
+                    let user_name = user_img.nextElementSibling;
+                    let createdAt = user_name.nextElementSibling;
+                    user_img.src = x.replies[i].user.image.png;
+                    user_name.innerText = x.replies[i].user.username;
+                    createdAt.innerText = x.replies[i].createdAt;
+                    cmnt_text.innerText = x.replies[i].content;
+                    reply_to.innerText = "@" + x.replies[i].replyingTo;
+                    cmnt_text.innerText = reply_to.innerText +" "+ x.replies[i].content;
+                }
+            }
         }
     }
     show();
+}
+
+function getInputDiv() {
+    
+}
+
+const myCommentDiv = document.createElement('div');
+const img_elem = document.createElement('img');
+const textarea_elem = document.createElement('textarea');
+const button_elem = document.createElement('button');
+img_elem.src = "./images/avatars/image-juliusomo.png";
+myCommentDiv.className = 'comment-input';
+textarea_elem.id = "comment-box";
+textarea_elem.placeholder = "Add a comment...";
+textarea_elem.rows = "5.5";
+textarea_elem.cols = "10";
+button_elem.innerText = "SEND";
+myCommentDiv.appendChild(img_elem);
+myCommentDiv.appendChild(textarea_elem);
+myCommentDiv.appendChild(button_elem);
+
+
+
+const reply_btn = document.getElementsByClassName('reply-btn');
+
+for (let i = 0; i < reply_btn.length; i++) {
+    reply_btn[i].onclick = function() {
+        window.scrollBy(0, 100);
+        myCommentDiv.remove();
+        textarea_elem.value = "";
+        let parentDiv = reply_btn[i].parentElement;
+        if (parentDiv.classList.contains('reply-cmnt')) {
+            myCommentDiv.style.width = "90%";
+            myCommentDiv.style.alignSelf = "flex-end";
+            parentDiv.after(myCommentDiv)
+        } else {
+            myCommentDiv.style.width = "100%";
+            myCommentDiv.style.alignSelf = "flex-start";
+            let last = parentDiv;
+            while(last.nextElementSibling) {
+                if (last.nextElementSibling.classList.contains("reply-cmnt")) {
+                    last = last.nextElementSibling;
+                } else {
+                    break;
+                }
+            }
+            if (last.classList.contains("reply-cmnt")) {
+                last.after(myCommentDiv);
+            } else {
+                last.after(myCommentDiv);
+            }
+        }
+    }
+}
+
+let myReplyComment = document.getElementsByClassName('reply-cmnt')[0];
+let clonedNode = myReplyComment.cloneNode(true);
+
+let sendButton = myCommentDiv.getElementsByTagName('button')[0];
+
+const container = document.getElementsByClassName('container')[0];
+
+sendButton.onclick = function() {
+    let main_elem = myCommentDiv.previousElementSibling;
+    main_elem.after(clonedNode)
+    myCommentDiv.remove()
 }
