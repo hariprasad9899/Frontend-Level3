674108ad0442456383ea8de3072fb234a2b63219
hariprasad9899 <hariprasaadsoundararajan@gmail.com>
2022-04-09 22:10:29 +0530

    reply comments done

--- interactive-comments-section-main/app_two.js
+++ interactive-comments-section-main/app_two.js
@@ -1,27 +1,30 @@
 
-
 // upvote and downvote
-let vote = document.getElementsByClassName('vote');
-
-for (let i = 0; i < vote.length; i++) {
-    let vote_count = 0;
-    let eval = vote[i].getElementsByClassName('eval');
-    for (let i = 0; i < eval.length; i++) {
-        eval[i].addEventListener('click', ()=> {
-            if(eval[i].classList.contains('add')) {
-                vote_count += 1;
-                let vote_val = eval[i].nextElementSibling;
-                vote_val.innerText = vote_count;
-            } else if (eval[i].classList.contains('sub')) {
-                let vote_val = eval[i].previousElementSibling;
-                if (vote_val.innerText > 0) {
-                    vote_count -= 1;
+let voting = function() {
+var vote = document.getElementsByClassName('vote');
+    for (let i = 0; i < vote.length; i++) {
+        let vote_count = 0;
+        let eval = vote[i].getElementsByClassName('eval');
+        for (let i = 0; i < eval.length; i++) {
+            eval[i].addEventListener('click', ()=> {
+                if(eval[i].classList.contains('add')) {
+                    vote_count += 1;
+                    let vote_val = eval[i].nextElementSibling;
                     vote_val.innerText = vote_count;
+                } else if (eval[i].classList.contains('sub')) {
+                    let vote_val = eval[i].previousElementSibling;
+                    if (vote_val.innerText > 0) {
+                        vote_count -= 1;
+                        vote_val.innerText = vote_count;
+                    }
                 }
-            }
-        })
+            })
+        }
     }
 }
+
+
+voting();
 
 // adding default comments from json data onload 
 var comment = document.getElementsByClassName('comment');
@@ -71,14 +74,12 @@ window.onload = function() {
     show();
 }
 
-function getInputDiv() {
-    
-}
 
 const myCommentDiv = document.createElement('div');
 const img_elem = document.createElement('img');
 const textarea_elem = document.createElement('textarea');
 const button_elem = document.createElement('button');
+myCommentDiv.className = "comment-input";
 img_elem.src = "./images/avatars/image-juliusomo.png";
 myCommentDiv.className = 'comment-input';
 textarea_elem.id = "comment-box";
@@ -90,49 +91,77 @@ myCommentDiv.appendChild(img_elem);
 myCommentDiv.appendChild(textarea_elem);
 myCommentDiv.appendChild(button_elem);
 
-
-
-const reply_btn = document.getElementsByClassName('reply-btn');
-
-for (let i = 0; i < reply_btn.length; i++) {
-    reply_btn[i].onclick = function() {
-        window.scrollBy(0, 100);
-        myCommentDiv.remove();
-        textarea_elem.value = "";
-        let parentDiv = reply_btn[i].parentElement;
-        if (parentDiv.classList.contains('reply-cmnt')) {
-            myCommentDiv.style.width = "90%";
-            myCommentDiv.style.alignSelf = "flex-end";
-            parentDiv.after(myCommentDiv)
-        } else {
-            myCommentDiv.style.width = "100%";
-            myCommentDiv.style.alignSelf = "flex-start";
-            let last = parentDiv;
-            while(last.nextElementSibling) {
-                if (last.nextElementSibling.classList.contains("reply-cmnt")) {
-                    last = last.nextElementSibling;
+let main_func = function() {
+    var cmnt_wrapper = document.getElementsByClassName('cmnt-wrapper');
+    var cmnt_type = "";
+    for(let j = 0; j < cmnt_wrapper.length; j++) {
+        const reply_btn = cmnt_wrapper[j].getElementsByClassName('reply-btn');
+        for (let i = 0; i <reply_btn.length;i++) {
+            reply_btn[i].addEventListener('click', () => {
+                myCommentDiv.remove()
+                textarea_elem.value = "";
+                if (reply_btn[i].parentElement.classList.contains('reply-cmnt')) {
+                    let commentDiv = reply_btn[i].parentElement;
+                    myCommentDiv.style.width = "90%";
+                    myCommentDiv.style.alignSelf = "flex-end";
+                    commentDiv.after(myCommentDiv);
+                    console.log(commentDiv);
+                    cmnt_type = "replied";
                 } else {
-                    break;
+                    let commentDiv = reply_btn[i].parentElement.parentElement;
+                    myCommentDiv.style.width = "100%";
+                    myCommentDiv.style.alignSelf = "flex-start";
+                    commentDiv.appendChild(myCommentDiv)
+                    cmnt_type = "comment";
                 }
-            }
-            if (last.classList.contains("reply-cmnt")) {
-                last.after(myCommentDiv);
-            } else {
-                last.after(myCommentDiv);
-            }
+            })
         }
+    }
+    return cmnt_type;
+}
+
+main_func();
+
+let myReplyComment = document.getElementsByClassName('reply-cmnt')[1];
+let clonedNode = myReplyComment.cloneNode(true);
+const btn_elem = myCommentDiv.getElementsByTagName('button');
+
+for (let i = 0; i < btn_elem.length; i++) {
+    btn_elem[i].onclick = function() {
+        if(textarea_elem.value != "") {
+            myReplyNode = clonedNode.cloneNode(true);
+            let replyDiv = myReplyNode.getElementsByClassName('cmnt-text')[0]
+            let replyText = replyDiv.getElementsByTagName('p')[0];
+            let user_time = myReplyNode.getElementsByClassName("user-time")[0];
+            let img_elem = user_time.getElementsByTagName('img')[0];
+            let name_elem = user_time.getElementsByTagName('span')[0];
+            let time_elem = user_time.getElementsByTagName('span')[1];
+            img_elem.src = "./images/avatars/image-juliusomo.png";
+            replyText.value = textarea_elem.value;
+            name_elem.innerText = "juliusomo";
+            time_elem.innerText = "1 Min ago";
+            let cmnt_type = main_func();
+            if(cmnt_type == "comment") {
+                let cmnt_wrapper = btn_elem[i].parentElement.parentElement;
+                cmnt_wrapper.appendChild(myReplyNode);
+                main_func()
+                voting();
+            } else {
+                let cmnt_wrapper_rep = btn_elem[i].parentElement.previousElementSibling;
+                cmnt_wrapper_rep.after(myReplyNode);
+                main_func()
+                voting();
+            }
+            myCommentDiv.remove();
+        }   
     }
 }
 
-let myReplyComment = document.getElementsByClassName('reply-cmnt')[0];
-let clonedNode = myReplyComment.cloneNode(true);
 
-let sendButton = myCommentDiv.getElementsByTagName('button')[0];
 
-const container = document.getElementsByClassName('container')[0];
 
-sendButton.onclick = function() {
-    let main_elem = myCommentDiv.previousElementSibling;
-    main_elem.after(clonedNode)
-    myCommentDiv.remove()
-}
+
+
+
+
+
