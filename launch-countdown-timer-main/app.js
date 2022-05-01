982aeb4e11dfb58555fcea355bfcfff777024d65
hariprasad9899 <hariprasaadsoundararajan@gmail.com>
2022-05-01 15:46:47 +0530

    basic styline done

--- launch-countdown-timer-main/app.js
+++ launch-countdown-timer-main/app.js
@@ -4,20 +4,22 @@ let minutes = document.getElementById('minutes');
 let seconds = document.getElementById('seconds');
 
 
-const launchDate = new Date("May 15, 2022 00:00:00");
+const launchDate = new Date("May 09, 2022 00:00:00");
 
-
-
-setInterval(()=> {
+function updateTime() {
     let presentDate = new Date();
     let difftime = Math.abs(launchDate - presentDate);
     let diffDay = Math.ceil(difftime / (1000 * 60 * 60 * 24))
     let diffHour = Math.ceil((difftime / (1000 * 60 * 60 )) % 24)
     let diffMinutes = Math.ceil((difftime/ (1000 * 60)) % 60);
     let diffSeconds = Math.ceil((difftime / 1000) % 60);
-    days.innerText = diffDay;
-    hours.innerText = diffHour
-    minutes.innerText = diffMinutes;
-    seconds.innerText = diffSeconds;
-},1000)
+    (diffDay.toString().length == 1) ? days.innerText = "0" + diffDay : days.innerText = diffDay;
+    (diffHour.toString().length == 1) ? hours.innerText = "0" + diffHour : hours.innerText = diffHour;
+    (diffMinutes.toString().length == 1) ? minutes.innerText = "0" + diffMinutes : minutes.innerText = diffMinutes;
+    (diffSeconds.toString().length == 1) ? seconds.innerText = "0" + diffSeconds : seconds.innerText = diffSeconds;
+}
+
+// setInterval(()=> {
+//     updateTime();
+// },1000)
 
